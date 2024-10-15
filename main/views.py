9f54d484a1a65056f0f1c89303025f63cf4e0d1c
Ethan Saso <ethansaso@gmail.com>
2024-10-15 11:50:40 -0700

    Updated bootstrap, switched to path-based routing for inputted cities, added error handling, moved to get requests

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from urllib.error import HTTPError
 from django.shortcuts import render
 import os
 import urllib
@@ -5,25 +6,31 @@ import json
 
 # Create your views here.
 def index(request):
-    if request.method == 'POST':
-        # Make request for city given in POST body
-        city = urllib.parse.quote(request.POST['city'])
+    return render(request, "main/index.html")
+
+def weather(request, city):
+    if request.method == 'GET':
+        # Make request for city given in GET body
+        city = urllib.parse.quote(city.replace('-', ' '))
+        units = request.GET.get('units', 'standard')
         WEATHER_API_KEY = os.environ['WEATHER_API_KEY']
-        source = urllib.request.urlopen(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}').read()
+        try:
+            source = urllib.request.urlopen(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}&units={units}').read()
 
-        # Convert JSON to dict
-        list_of_data = json.loads(source)
+            # Convert JSON to dict
+            list_of_data = json.loads(source)
 
-        data = {
-            "country_code": str(list_of_data['sys']['country']), 
-            "coordinate": str(list_of_data['coord']['lon']) + ' '
-                        + str(list_of_data['coord']['lat']), 
-            "temp": str(list_of_data['main']['temp']) + 'k', 
-            "pressure": str(list_of_data['main']['pressure']), 
-            "humidity": str(list_of_data['main']['humidity']), 
-        }
-        
-        print(data)
+            data = {
+                "city": urllib.parse.unquote(city).title(),
+                "country_code": str(list_of_data['sys']['country']), 
+                "coordinate": str(list_of_data['coord']['lon']) + ' '
+                            + str(list_of_data['coord']['lat']), 
+                "temp": str(list_of_data['main']['temp']) + (' °C' if units == 'metric' else ' K'),
+                "pressure": str(list_of_data['main']['pressure']), 
+                "humidity": str(list_of_data['main']['humidity']), 
+            }
+        except HTTPError:
+            data = {"error": '404', "city": city}
     else:
         data = {}
-    return render(request, "main/index.html", data)
+    return render(request, "main/weather.html", data)
