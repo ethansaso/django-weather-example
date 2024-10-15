184a995c13f35bcf713673009c26be99c1f8f40a
Ethan Saso <ethansaso@gmail.com>
2024-10-15 14:39:35 -0700

    Implemented dropdown for language, switched to AJAX design for SPA approach

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [ 
     path('', views.index, name='index'), 
     path('<str:city>', views.weather, name='weather'), 
+    path('update-weather-params/<str:city>', views.update_weather_params, name='update_weather_params'), 
 ] 
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from urllib.error import HTTPError
 from django.shortcuts import render
+from django.http import JsonResponse
 import os
 import urllib
 import json
@@ -12,20 +13,22 @@ def weather(request, city):
     if request.method == 'GET':
         # Make request for city given in GET body
         city = urllib.parse.quote(city.replace('-', ' '))
-        units = request.GET.get('units', 'standard')
+        units = request.GET.get('units', 'imperial')
+        lang = request.GET.get('lang', 'en')
         WEATHER_API_KEY = os.environ['WEATHER_API_KEY']
         try:
-            source = urllib.request.urlopen(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}&units={units}').read()
+            source = urllib.request.urlopen(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}&units={units}&lang={lang}').read()
 
             # Convert JSON to dict
             list_of_data = json.loads(source)
 
             data = {
-                "city": urllib.parse.unquote(city).title(),
+                "city": str(list_of_data['name']),
                 "country_code": str(list_of_data['sys']['country']), 
                 "coordinate": str(list_of_data['coord']['lon']) + ' '
                             + str(list_of_data['coord']['lat']), 
-                "temp": str(list_of_data['main']['temp']) + (' °C' if units == 'metric' else ' K'),
+                "description": str(list_of_data['weather'][0]['description']),
+                "temp": str(list_of_data['main']['temp']) + (' °C' if units == 'metric' else ' °F'),
                 "pressure": str(list_of_data['main']['pressure']), 
                 "humidity": str(list_of_data['main']['humidity']), 
             }
@@ -34,3 +37,23 @@ def weather(request, city):
     else:
         data = {}
     return render(request, "main/weather.html", data)
+
+def update_weather_params(request, city):
+    city = urllib.parse.quote(city.replace('-', ' '))
+    units = request.GET.get('units', 'imperial')
+    lang = request.GET.get('lang', 'en')
+
+    WEATHER_API_KEY = os.environ['WEATHER_API_KEY']
+    
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}&units={units}&lang={lang}"
+    print(url)
+    source = urllib.request.urlopen(url).read()
+
+    # Convert JSON to dict
+    data = json.loads(source)
+
+    return JsonResponse({
+        "temp": f"{data['main']['temp']} {' °C' if units == 'metric' else ' °F'}",
+        "description": f"{data['weather'][0]['description']}",
+        "name": f"{data['name']}"
+    })
